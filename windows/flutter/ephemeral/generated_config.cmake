8c95284bebe5e44e79ff31ae47474034e6a826da
PeeyushKulgude <peeyush.kulgude777@gmail.com>
2022-10-28 08:58:49 +0530

    Primitive version of personal expenses app

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\peeyu\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\peeyu\\Programming\\FlutterProjects\\expenses" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\peeyu\\flutter"
+  "PROJECT_DIR=C:\\Users\\peeyu\\Programming\\FlutterProjects\\expenses"
+  "FLUTTER_ROOT=C:\\Users\\peeyu\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\peeyu\\Programming\\FlutterProjects\\expenses\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\peeyu\\Programming\\FlutterProjects\\expenses"
+  "FLUTTER_TARGET=C:\\Users\\peeyu\\Programming\\FlutterProjects\\expenses\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\peeyu\\Programming\\FlutterProjects\\expenses\\.dart_tool\\package_config.json"
+)
